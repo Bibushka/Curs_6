c7db3562639861a5a96bfd9b057b74c5a0d8d8cb
Bibushka <bianca_bucur03@yahoo.com>
2017-10-21 12:15:04 +0300

    StringInString test: red

---
+++ String in string/String in string/StringInString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringInString
+{
+    [TestClass]
+    public class StringInString
+    {
+        [TestMethod]
+        public void IsStringCorrectlyComposed()
+        {
+            Assert.AreEqual("wordinword", InsertString("wordsword", "in", 4));
+        }
+
+        public string InsertString(string initialString, string stringToBeInserted, int position)
+        {
+            return "";
+        }
+    }
+}
