176badfd29e61819dcc6d258c8553b8bd3a8c9ee
Bibushka <bianca_bucur03@yahoo.com>
2017-10-21 11:30:48 +0300

    ReverseString test: red

---
+++ ReverseString/ReverseString/ReverseString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReverseString
+{
+    [TestClass]
+    public class ReverseString
+    {
+        [TestMethod]
+        public void TestStringReversal()
+        {
+            Assert.AreEqual("yppah", Reverse("happy",""));
+        }
+
+        public string Reverse(string initialString, string newString)
+        {
+            return "";
+        }
+    }
+}
