b70e5d98e1e8ecb6650fd294a7c7aacaf7cfabe8
Bibushka <bianca_bucur03@yahoo.com>
2017-10-22 13:27:57 +0300

    Triangle test: red

---
+++ Triangle/Triangle/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Triangle
+{
+    [TestClass]
+    public class Triangle
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 4, 6, 4, 1 }, GenerateTriangle(5));
+        }
+
+        public Array GenerateTriangle(int n)
+        {
+            return GetTriangleLine(n, 2, new int[] { 1, 1});
+        }
+
+        public Array GetTriangleLine(int n, int i, int[] line)
+        {
+            return new int[] { 0 };
+        }
+    }
+}
