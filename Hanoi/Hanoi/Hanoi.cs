b450a1e92482c32f2d544c3af407e805ac802681
Bibushka <bianca_bucur03@yahoo.com>
2017-11-01 22:17:24 +0200

    Hanoi test: red

---
+++ Hanoi/Hanoi/Hanoi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hanoi
+{
+    [TestClass]
+    public class Hanoi
+    {
+        [TestMethod]
+        public void GetHanoiResult()
+        {
+            Assert.AreEqual("AC AB CB AC BA BC AC", GetHanoiString(3));
+        }
+
+        public string GetHanoiString(int n)
+        {
+            string moves = string.Empty;
+            string a = "A";
+            string b = "B";
+            string c = "C";
+            return GetMoves(n, moves, a, c, b);
+        }
+
+        public string GetMoves(int n, string moves, string a, string b, string c)
+        {
+            return "";
+        }
+    }
+}
