f98a24fe86d27c20f885572b1bdc760340cb8a0d
Bibushka <bianca_bucur03@yahoo.com>
2017-11-03 22:39:14 +0200

    Calculator test: red

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(65, GetResult("+ 7 58"));
+        }
+
+        public string[] GetResult(string calculation)
+        {
+            string[] calculationString = calculation.Split(' ');
+
+            return calculationString;
+        }
+    }
+}
