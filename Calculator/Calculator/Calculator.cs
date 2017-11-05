f68314a833dafc07f612fcdddb4d6f340f363d60
Bibushka <bianca_bucur03@yahoo.com>
2017-11-05 16:52:39 +0200

    Calculator test: index error

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Calculator
 {
     [TestClass]
-    public class UnitTest1
+    public class Calculator
     {
         [TestMethod]
         public void TestMethod1()
@@ -12,11 +12,32 @@ namespace Calculator
             Assert.AreEqual(65, GetResult("+ 7 58"));
         }
 
-        public string[] GetResult(string calculation)
+        public int[] GetResult(string calculatorScreen)
         {
-            string[] calculationString = calculation.Split(' ');
+            string[] calculationString = calculatorScreen.Split(' ');
+            string[][] splitString = new string[2][];
+            splitString[0] = new string[calculationString.Length/2];
+            splitString[1] = new string[calculationString.Length / 2 + 1];
+            splitString = Split(calculationString, splitString, 0, 0);
+            string[] simbols = splitString[0];
+            int[] numbers = Array.ConvertAll(splitString[1], int.Parse);
+            return numbers;
+        }
 
-            return calculationString;
+        public string[][] Split(string[] calculationString, string[][] splitString, int i, int j)
+        {
+            string[] newString = new string[calculationString.Length-1];
+            if (calculationString.Length == 0)
+                return splitString;
+            if ("+-/*".Contains(calculationString[calculationString.Length - 1]))
+            {
+                splitString[0][i] = calculationString[calculationString.Length - 1];
+                Array.Copy(calculationString, newString, newString.Length);
+                return Split(newString, splitString, i + 1, j);
+            }
+            splitString[0][j] = calculationString[calculationString.Length - 1];
+            Array.Copy(calculationString, newString, newString.Length);
+            return Split(newString, splitString, i, j + 1);
         }
     }
 }
