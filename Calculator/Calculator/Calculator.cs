9e6c08e0dee7ea8010df30b00f1c45cc2bb9ea8d
Bibushka <bianca_bucur03@yahoo.com>
2017-11-12 21:33:46 +0200

    Calculator test: green

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -10,32 +10,66 @@ namespace Calculator
     public class Calculator
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SingleIntTest()
+        {
+            Assert.AreEqual(58, GetResult("58"));
+        }
+
+        [TestMethod]
+        public void SingleDoubleTest()
         {
             Assert.AreEqual(58.7, GetResult("58.7"));
         }
 
-        public float GetResult(string calculatorScreen)
+        [TestMethod]
+        public void SingleDoublePlusTest()
+        {
+            Assert.AreEqual(58.7, GetResult("+ 58.7"));
+        }
+
+
+        [TestMethod]
+        public void FullTest()
+        {
+            Assert.AreEqual(32.4, GetResult("/ + 6 58.8 2"));
+        }
+
+        public double GetResult(string calculatorScreen)
         {
             string[] calculationString = calculatorScreen.Split(' ');
-            float result = 0;
-            return Split(calculationString, result);
+            if (calculationString.Length <= 2)
+                return double.Parse(calculationString[calculationString.Length-1]);
+            return Split(calculationString, calculationString.Length/2-1 , calculationString.Length/2);
         }
 
-        public float Split(string[] calculationString, float result)
+        public double Split(string[] calculationString, int frontIndex, int backIndex)
         {
-            if (calculationString.Length == 0)
-                return result;
-            return Split(ResizeString(calculationString, calculationString.Length-1), 
-                result + float.Parse(calculationString[calculationString.Length - 1], System.Globalization.CultureInfo.InvariantCulture));
-        }
-
-        public string[] ResizeString(string[] yourString, int lenght)
-        {
-            Array.Reverse(yourString);
-            Array.Resize(ref yourString, lenght);
-            Array.Reverse(yourString);
-            return yourString;
+            if (frontIndex == -1)
+                return double.Parse(calculationString[backIndex]);
+            switch (calculationString[frontIndex])
+            {
+                case "+":
+                    calculationString[backIndex + 1] = 
+                        (double.Parse(calculationString[backIndex]) + 
+                        double.Parse(calculationString[backIndex+1])).ToString();
+                    break;
+                case "-":
+                    calculationString[backIndex + 1] =
+                        (double.Parse(calculationString[backIndex]) +
+                        double.Parse(calculationString[backIndex - 1])).ToString();
+                    break;
+                case "/":
+                    calculationString[backIndex + 1] =
+                        (double.Parse(calculationString[backIndex]) /
+                        double.Parse(calculationString[backIndex + 1])).ToString();
+                    break;
+                case "*":
+                    calculationString[backIndex + 1] =
+                        (double.Parse(calculationString[backIndex]) *
+                        double.Parse(calculationString[backIndex + 1])).ToString();
+                    break;
+            }
+            return Split(calculationString, frontIndex - 1, backIndex + 1);
         }
     }
 }
